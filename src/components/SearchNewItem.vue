da16f24c6f3a5e531c06a62c1acc70b1b399a66c
semanurakkuzu <semanurakkuzu@gmail.com>
2023-01-07 23:21:21 +0300

    add `SearchNewItem` component

---
+++ src/components/SearchNewItem.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <div class="card rounded-2">
+      <div class="card-header">
+        <div class="row align-items-center">
+          <div class="col">
+            <span>{{ coin.symbol }}</span>
+          </div>
+          <div class="col-auto">
+            <button class="btn btn-primary px ms-4" @click="addItem">
+              <i class="bi bi-plus-square"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="card-body">
+        <div class="card-text">
+          <div class="row align-items-center">
+            <div class="col">
+              <span>Last Price: </span>
+              <span>{{ coin.lastPrice }}</span>
+            </div>
+            <div class="col-auto">
+              <input
+                class="text-center input-bg-color"
+                type="number"
+                min="1"
+                max="9999"
+                v-model="inputNumberValue"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchNewAItem',
+  props: {
+    coin: {
+      type: Object
+    }
+  },
+  data() {
+    return {
+      inputNumberValue: 1
+    }
+  },
+  methods: {
+    addItem() {
+      this.$emit('add-item', this.coin.symbol, this.inputNumberValue)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.input-bg-color {
+  background: #e1c7ec;
+}
+
+.px {
+  padding-right: 36px;
+  padding-left: 36px;
+}
+</style>
